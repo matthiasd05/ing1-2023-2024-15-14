4e9e00e6faf5a29d96f3b0fdb9a82362a7734902
matthiasd05 <148857028+matthiasd05@users.noreply.github.com>
2023-11-10 17:13:15 +0100

    Add files via upload

--- SNOOPY/main.c
+++ SNOOPY/main.c
@@ -251,21 +251,31 @@ void chargerPartie(const char *nomFichier) {
 
 
 void murCassable(){
+    // Vérifie si le joueur a le droit de casser un mur
     if(droit_casser==1){
+        // Vérifie si Snoopy est à côté d'un mur cassable (en haut)
         if(Plateau[SnoopyX][SnoopyY+1]==Plateau[mur_cassableX][mur_cassableY]){
+            // Casse le mur en mettant à jour la case correspondante dans le plateau
             Plateau[mur_cassableX][mur_cassableY]=' ';
         }
+        // Vérifie si Snoopy est à côté d'un mur cassable (en dessous)
         else if(Plateau[SnoopyX][SnoopyY-1]==Plateau[mur_cassableX][mur_cassableY]){
+            // Casse le mur en mettant à jour la case correspondante dans le plateau
             Plateau[mur_cassableX][mur_cassableY]=' ';
         }
+        // Vérifie si Snoopy est à côté d'un mur cassable (a droite)
         else if(Plateau[SnoopyX+1][SnoopyY]==Plateau[mur_cassableX][mur_cassableY]){
+            // Casse le mur en mettant à jour la case correspondante dans le plateau
             Plateau[mur_cassableX][mur_cassableY]=' ';
         }
+        // Vérifie si Snoopy est à côté d'un mur cassable (a gauche)
         else if(Plateau[SnoopyX-1][SnoopyY]==Plateau[mur_cassableX][mur_cassableY]){
+            // Casse le mur en mettant à jour la case correspondante dans le plateau
             Plateau[mur_cassableX][mur_cassableY]=' ';
         }
     }
 }
+
 
 void mur(){
     static int droit_bouger=0;
