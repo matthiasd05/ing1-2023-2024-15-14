477f4471aa6c936586e7f9f1519d3be2587b51ff
Lise <148774777+Alcyone0@users.noreply.github.com>
2023-11-10 19:26:59 +0100

    Update main.c

--- SNOOPY/main.c
+++ SNOOPY/main.c
@@ -285,32 +285,49 @@ void murCassable(){
 
 void mur(){
     static int droit_bouger=0;
+    //Vérifie si le joueur a le droit de bouger un mur
     if(droit_bouger==0){
+        //Vérifie si Snoopy est à côté d'un mur poussable (en haut)
         if(Plateau[SnoopyX][SnoopyY+1]==Plateau[murX][murY] && droit_bouger==0){
             Plateau[murX][murY]=' ' ;
+            //Remplace l'ancienne case par un espace vide
             murY++;
+            //Ajoute 1 à la coordonnée en Y (ordonnée du mur)
             Plateau[murX][murY]= 'M';
+            //Mets la lettre M sur la nouvelle case
             droit_bouger=1;
             droit_pousser=1;
         }
+        //Vérifie si Snoopy est à côté d'un mur poussable (en dessous)
         else if(Plateau[SnoopyX][SnoopyY-1]==Plateau[murX][murY] && droit_bouger==0){
             Plateau[murX][murY]=' ' ;
+            //Remplace l'ancienne case par un espace vide
             murY--;
+            //Soustrait 1 à la coordonnée en Y (ordonnée du mur)
             Plateau[murX][murY]= 'M';
+            //Mets la lettre M sur la nouvelle case 
             droit_bouger=1;
             droit_pousser=1;
         }
+        //Vérifie si Snoopy est à côté d'un mur poussable (à droite)
         else if(Plateau[SnoopyX+1][SnoopyY]==Plateau[murX][murY] && droit_bouger==0){
             Plateau[murX][murY]=' ' ;
+            //Remplace l'ancienne case par un espace vide
             murX++;
+            //Ajoute 1 à la coordonnée en X (ordonnée du mur)
             Plateau[murX][murY]= 'M';
+            //Mets la lettre M sur la nouvelle case 
             droit_bouger=1;
             droit_pousser=1;
         }
+        //Vérifie si Snoopy est à côté d'un mur poussable (à gauche)
         else if(Plateau[SnoopyX-1][SnoopyY]==Plateau[murX][murY] && droit_bouger==0){
             Plateau[murX][murY]=' ' ;
+            //Remplace l'ancienne case par un espace vide
             murX--;
+            //Soustrait 1 à la coordonnée en X (ordonnée du mur)
             Plateau[murX][murY]= 'M';
+            //Mets la lettre M sur la nouvelle case 
             droit_bouger=1;
             droit_pousser=1;
         }
